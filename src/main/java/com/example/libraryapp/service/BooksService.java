448f65662dddb5fb9a3a8baa5eb8ad2faad69040
Carlos479067 <carlos.martinez11@my.utsa.edu>
2025-05-14 00:38:48 -0500

    Created controller, repository, and service class for model class Book. getAllBooks method added.

--- src/main/java/com/example/libraryapp/service/BooksService.java
+++ src/main/java/com/example/libraryapp/service/BooksService.java
@@ -2,7 +2,6 @@ package com.example.libraryapp.service;
 import com.example.libraryapp.model.Book;
 import com.example.libraryapp.repository.BooksRepository;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
