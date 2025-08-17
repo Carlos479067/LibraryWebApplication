22365a5cb015376ebf155e292caa399c7f4d3060
Carlos479067 <carlos.martinez11@my.utsa.edu>
2025-08-16 23:48:37 -0500

    Created header with simple logo and color palette

---
+++ library-react/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className={"header"}>
+            <img src={"src/assets/books.png"} alt={"books logo"}/>
+            <h1>Library Application</h1>
+        </header>
+    )
+}
