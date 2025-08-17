aed31bb4a0d3eab49d8661dec9cbd64615856e3a
Carlos479067 <carlos.martinez11@my.utsa.edu>
2025-08-16 23:57:53 -0500

    In App.jsx Passed updateState as a prop to NavBar to be used.     Navbar fetches the data from user and calls updateState(data).     updateState updates the state and now searchResults holds the data.     searchResults(data) will now be sent to Main as a prop to be used.

--- library-react/src/App.jsx
+++ library-react/src/App.jsx
@@ -1,15 +1,32 @@
 import './App.css'
-import TestButton from "./components/TestButton.jsx";
+import './index.css'
+import NavBar from '../src/components/Navbar.jsx';
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
+import {useState} from "react";
 
 function App() {
 
+    const[searchResults, setSearchResults] = useState([]);
+
+    function UpdateState(data) {
+        setSearchResults(data);
+    }
+
   return (
-    <>
-      <div>
-        <TestButton></TestButton>
-      </div>
-    </>
+      <>
+        <Header/>
+        <NavBar updateState={UpdateState}/>
+          <Main searchResults={searchResults}/>
+      </>
   )
 }
+
+/*
+    Passed updateState as a prop to NavBar to be used.
+    Navbar fetches the data from user and calls updateState(data).
+    updateState updates the state and now searchResults holds the data.
+    searchResults(data) will now be sent to Main as a prop to be used.
+ */
 
 export default App
